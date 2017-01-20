d5d3e76e0fecdccab47697dbbf51027f8a6bbca0
aoki.makoto <aoki_makoto@ninton.co.jp>
2017-01-21 01:51:08 +0900

    Fixed jslint error.

--- content/extensions/gosub-sel-ide.js
+++ content/extensions/gosub-sel-ide.js
@@ -1,3 +1,5 @@
+/*jslint white: true */
+/*global Selenium, testCase, LOG*/
 /**
  * gosub-sel-ide.js
  * A gosub control plugin for Selenium-IDE
@@ -7,15 +9,17 @@
  * It referred to Selenium IDE: Flow Control
  */
 function GosubControlClass() {
+    "use strict";
+
     this.labels   = {};
     this.stack    = [];
     this.init_err = null;
 
-    this.initOnce = function ( io_selenium ) {
+    this.initOnce = function (io_selenium) {
         // issue #2: If "Flow Control" is installed, this cannot be initialized correctly
         // [MEMO] htmlTestRunner.metrics.startTime is undefined.
         // [MEMO] testCase.lastModifiedTime is same as testCase.file.lastModifiedTime.
-        if ( typeof io_selenium.gosub_initialized != 'undefined' ) {
+        if (io_selenium.gosub_initialized !== undefined) {
             return;
         }
 
@@ -24,85 +28,89 @@ function GosubControlClass() {
     };
 
     this.init = function () {
-        this.echo( 'gosubInit' );
-        
+        this.echo('gosubInit');
+
         this.labels   = {};
         this.stack    = [];
         this.init_err = null;
-        
+
         try {
-            this._init();
+            this.initBody();
         } catch (e) {
             this.init_err = e;
         }
     };
 
-    this._init = function () {
-        var lbl = '';
-        var idx = -1;
+    this.initBody = function () {
+        var lbl = '',
+            idx = -1,
+            i,
+            cmd;
 
-        for( var i = 0; i < testCase.commands.length; i++ ) {
-            var cmd = testCase.commands[i];
+        for (i = 0; i < testCase.commands.length; i += 1) {
+            cmd = testCase.commands[i];
 
-            if ( 'command' == cmd.type ) {
-                switch( cmd.command.toLowerCase() ) {
+            if ('command' === cmd.type) {
+                switch (cmd.command.toLowerCase()) {
                     case "sub":
-                        if ( lbl !== '' ) {
-                            throw new Error( 'There is no "endsub" corresponding to "sub": line=' + i );
+                        if (lbl !== '') {
+                            throw new Error('There is no "endsub" corresponding to "sub": line=' + i);
                         }
                         lbl = cmd.target;
                         idx = i;
-                        if ( lbl === '' ) {
-                            throw new Error( 'A label of "sub" is a blank: line=' + i );
+                        if (lbl === '') {
+                            throw new Error('A label of "sub" is a blank: line=' + i);
                         }
-                        if ( typeof this.labels[lbl] != 'undefined' ) {
-                            throw new Error( 'A label of "sub" appears twice or more: line=' + i );
+                        if (this.labels[lbl] !== undefined) {
+                            throw new Error('A label of "sub" appears twice or more: line=' + i);
                         }
                         break;
 
                     case "endsub":
-                        if ( lbl === '' ) {
-                            throw new Error( 'There is no "sub" corresponding to "endsub": line=' + i );
+                        if (lbl === '') {
+                            throw new Error('There is no "sub" corresponding to "endsub": line=' + i);
                         }
-                        this.labels[lbl] = {sub:idx, end:i};
+                        this.labels[lbl] = {sub: idx, end: i};
                         lbl = '';
                         idx = -1;
                         break;
                 }
             }
         }
-        if ( lbl !== '' ) {
-            throw new Error( 'There is no "endsub" corresponding to "sub": line=' + i );
+        if (lbl !== '') {
+            throw new Error('There is no "endsub" corresponding to "sub": line=' + i);
         }
     };
 
-    this.echo = function ( i_mesg ) {
-        LOG.info( i_mesg );
+    this.echo = function (i_mesg) {
+        LOG.info(i_mesg);
     };
 
-    this.gotoSub = function ( i_label ) {
-        if ( undefined === this.labels[i_label] ) {
-            throw new Error( '"sub ' + i_label + '" is not found.' );
+    this.gotoSub = function (i_label) {
+        var idx;
+
+        if (undefined === this.labels[i_label]) {
+            throw new Error('"sub ' + i_label + '" is not found.');
         }
 
-        var idx = parseInt(this.labels[i_label].sub) - 1;
-        this.setIndex( idx );
+        idx = parseInt(this.labels[i_label].sub, 10) - 1;
+        this.setIndex(idx);
         return idx;
     };
 
-    this.gotoEndsub = function ( i_label ) {
-        if ( undefined === this.labels[i_label] ) {
-            throw new Error( '"sub ' + i_label + '" is not found.' );
+    this.gotoEndsub = function (i_label) {
+        if (undefined === this.labels[i_label]) {
+            throw new Error('"sub ' + i_label + '" is not found.');
         }
 
-        var idx = parseInt(this.labels[i_label].end) - 1;
-        this.setIndex( idx );
+        var idx = parseInt(this.labels[i_label].end, 10) - 1;
+        this.setIndex(idx);
         return idx;
     };
 
-    this.setIndex = function ( i_value ) {
+    this.setIndex = function (i_value) {
         if (undefined === i_value || null === i_value || i_value < 0) {
-            throw new Error( "Invalid index: index=" + i_value );
+            throw new Error("Invalid index: index=" + i_value);
         }
         testCase.debugContext.debugIndex = i_value;
     };
@@ -112,46 +120,50 @@ function GosubControlClass() {
     };
 
     this.debug = function () {
-        this.echo( 'gosubDebug' );
+        this.echo('gosubDebug');
 
-        for ( var label in this.labels ) {
-            this.echo( "gosubDebug:sub|" + label + "|" + this.labels[label].sub + "|" + this.labels[label].end);
-        }
+        Object.keys(this.labels).forEach(function(label) {
+            this.echo("gosubDebug:sub|" + label + "|" + this.labels[label].sub + "|" + this.labels[label].end);
+        });
     };
 
-    this.doGosub = function ( i_label ) {
-        if ( this.init_err !== null ) {
+    this.doGosub = function (i_label) {
+        if (this.init_err !== null) {
             throw this.init_err;
         }
 
-        this.stack.push( this.getIndex() );
-        this.gotoSub( i_label );
+        this.stack.push(this.getIndex());
+        this.gotoSub(i_label);
     };
 
-    this.doSub = function ( i_label ) {
-        if ( this.init_err !== null ) {
+    this.doSub = function (i_label) {
+        var idx;
+
+        if (this.init_err !== null) {
             throw this.init_err;
         }
 
-        if ( 0 === this.stack.length ) {
-            var idx = this.gotoEndsub( i_label );
-            this.echo( "sub : fall down, skip to line=" +  idx );
+        if (0 === this.stack.length) {
+            idx = this.gotoEndsub(i_label);
+            this.echo("sub : fall down, skip to line=" +  idx);
         } else {
-            this.echo( "sub : from gosub" );
+            this.echo("sub : from gosub");
         }
     };
 
     this.doEndsub = function () {
-        if ( this.init_err !== null ) {
+        var idx;
+
+        if (this.init_err !== null) {
             throw this.init_err;
         }
 
-        if ( 0 === this.stack.length ) {
-            this.echo( "endsub : fall down" );
+        if (0 === this.stack.length) {
+            this.echo("endsub : fall down");
         } else {
-            var idx = this.stack.pop();
-            this.echo( "endsub : goto line=" + idx );
-            this.setIndex( idx );
+            idx = this.stack.pop();
+            this.echo("endsub : goto line=" + idx);
+            this.setIndex(idx);
         }
     };
 }
@@ -159,31 +171,37 @@ function GosubControlClass() {
 var GosubControl = new GosubControlClass();
 
 
-Selenium.prototype.doGosubInit = function() {
+Selenium.prototype.doGosubInit = function () {
+    "use strict";
     GosubControl.init();
 };
 
-Selenium.prototype.doGosubDebug = function() {
+Selenium.prototype.doGosubDebug = function () {
+    "use strict";
     GosubControl.initOnce(this);
     GosubControl.debug();
 };
 
-Selenium.prototype.doGosub = function( i_label ) {
+Selenium.prototype.doGosub = function (i_label) {
+    "use strict";
     GosubControl.initOnce(this);
-    GosubControl.doGosub( i_label );
+    GosubControl.doGosub(i_label);
 };
 
-Selenium.prototype.doSub = function( i_label ) {
+Selenium.prototype.doSub = function (i_label) {
+    "use strict";
     GosubControl.initOnce(this);
-    GosubControl.doSub( i_label );
+    GosubControl.doSub(i_label);
 };
 
-Selenium.prototype.doEndsub = function() {
+Selenium.prototype.doEndsub = function () {
+    "use strict";
     GosubControl.initOnce(this);
     GosubControl.doEndsub();
 };
 
-Selenium.prototype.doReturn = function() {
+Selenium.prototype.doReturn = function () {
+    "use strict";
     GosubControl.initOnce(this);
     GosubControl.doEndsub();
 };
